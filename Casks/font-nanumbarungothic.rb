6cdf8fc0f978359477e8621903105275645c71da
Jaeho Lee <jaeho.lee@snu.ac.kr>
2019-06-25 04:21:48 +0900

    :tada: Initial commit

---
+++ Casks/font-nanumbarungothic.rb
@@ -0,0 +1,11 @@
+cask 'font-nanumbarungothic' do
+    version :latest
+    sha256 :no_check
+    url "https://codeload.github.com/Zeta611/fonts/zip/master"
+    name 'Nanumbarungothic'
+    homepage 'https://hangeul.naver.com/2017/nanum'
+    font "fonts-master/nanumbarungothic/NanumBarunGothicUltraLight.otf"
+    font "fonts-master/nanumbarungothic/NanumBarunGothicLight.otf"
+    font "fonts-master/nanumbarungothic/NanumBarunGothicBold.otf"
+    font "fonts-master/nanumbarungothic/NanumBarunGothic.otf"
+end
